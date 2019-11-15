01a02aa7c9355eece0a0f87164b80c660ebc35d6
Morten Gjesing <mortengjesing@users.noreply.github.com>
2019-11-15 10:48:49 +0100

    Add Nuke build script

---
+++ build/Build.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI.TeamCity;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target Nuget => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetSources(RootDirectory / "Hilma.Domain" + "Hilma.Domain.csproj")
+                .SetOutputDirectory(ArtifactsDirectory)
+                .SetNoBuild(true)
+            );
+
+            if (TeamCity.Instance != null)
+            {
+                TeamCity.Instance.PublishArtifacts(ArtifactsDirectory  / "Hilma.Domain.1.0.0.nupkg");
+            }
+        });
+}
