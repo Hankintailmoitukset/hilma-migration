563d0432c55cd4a1bed8e46b33bea752c2d8091e
Jan Madsen <jalm@mercell.com>
2019-11-19 12:52:18 +0100

    Added tests form most used forms

---
+++ Hilma.Tests/NoticeImportContract.cs
@@ -0,0 +1,23 @@
+﻿// Responsible developer:
+// Responsible team:
+
+using System;
+using Hilma.Domain.Integrations.HilmaMigration;
+
+namespace Hilma.Tests
+{
+    public class NoticeImportContract : INoticeImportModel
+    {
+        public string NoticeNumber { get; set; }
+        public string FormNumber { get; set; }
+        public string Notice { get; set; }
+        public string TedSubmissionId { get; set; }
+        public string NoticeOjsNumber { get; set; }
+        public string NoticeType { get; set; }
+        public DateTime? HilmaSubmissionDate { get; set; }
+        public DateTime? HilmaPublishedDate { get; set; }
+        public bool IsPublishedInTed { get; set; }
+        public DateTime? TedPublishedDate { get; set; }
+        public string PreviousNoticeNumber { get; set; }
+    }
+}
---
+++ Hilma.Tests/TestHelpers.cs
@@ -0,0 +1,32 @@
+﻿// Responsible developer:
+// Responsible team:
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Hilma.Tests
+{
+    public static class TestHelpers
+    {
+        public static string GetEmbeddedResourceAsString(string endsWith, Assembly assembly = null)
+        {
+            if (assembly == null)
+            {
+                assembly = Assembly.GetCallingAssembly();
+            }
+
+            var name = assembly.GetManifestResourceNames().First(s => s.EndsWith(endsWith));
+            string result;
+
+            using (Stream stream = assembly.GetManifestResourceStream(name))
+            using (StreamReader reader = new StreamReader(stream ?? throw new InvalidOperationException()))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+    }
+}
