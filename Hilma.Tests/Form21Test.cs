fcc4d028de2a37d94b5c660804d2576d5f37075c
Jan Madsen <jalm@mercell.com>
2021-03-30 15:44:09 +0200

    addded form 21 test

--- Hilma.Tests/Form21Test.cs
+++ Hilma.Tests/Form21Test.cs
@@ -18,5 +18,13 @@ namespace Hilma.Tests
             var tedXml = TestHelpers.ValidateFormReturnTedXml("21", "contract", formOriginalXml);
             Assert.IsNotNull(tedXml);
         }
+
+        [TestMethod]
+        public void TestForm21_2()
+        {
+            var formOriginalXml = TestHelpers.GetEmbeddedResourceAsString($"Form21_2.xml");
+            var tedXml = TestHelpers.ValidateFormReturnTedXml("21", "contract", formOriginalXml);
+            Assert.IsNotNull(tedXml);
+        }
     }
 }
